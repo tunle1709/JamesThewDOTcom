6ccd4cbb7c351f970ae080cc786ce80c962c1381
tuanlm <tuan.lm.2128@aptechlearning.edu.vn>
2024-01-10 18:40:46 +0700

    View Login and Register demo

---
+++ JamesThewDOTcom/JamesThewDOTcom/Controllers/UserAuthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JamesThewDOTcom.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
